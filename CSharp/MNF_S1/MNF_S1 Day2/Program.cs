6f4be063970807a2274842d6c9433c9facf7950f
Ghaly Samir <168774425+ghalysamir@users.noreply.github.com>
2025-07-29 00:30:15 +0300

    Add files via upload

---
+++ CSharp/MNF_S1/MNF_S1 Day2/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MNF_S1_Day2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
